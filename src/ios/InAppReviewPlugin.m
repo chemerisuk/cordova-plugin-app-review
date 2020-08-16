879099ba33e8d0720bc73a468352bc1b5941bc7b
Maksim Chemerisuk <chemerisuk@gmail.com>
2020-08-17 00:43:54 +0300

    implement requestReviewManually for iOS

--- src/ios/InAppReviewPlugin.m
+++ src/ios/InAppReviewPlugin.m
@@ -17,7 +17,23 @@
 }
 
 - (void)requestReviewManually:(CDVInvokedUrlCommand*)command {
+    NSString* bundleId = [[NSBundle mainBundle] infoDictionary][@"CFBundleIdentifier"];
+    NSString* lookupURL = [NSString stringWithFormat:@"http://itunes.apple.com/lookup?bundleId=%@", bundleId];
+    NSData* data = [NSData dataWithContentsOfURL:[NSURL URLWithString:lookupURL]];
+    NSDictionary* lookup = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
 
+    CDVPluginResult* pluginResult;
+    if ([lookup[@"resultCount"] integerValue] == 1) {
+        NSString* trackId = lookup[@"results"][0][@"trackId"];
+        NSString* reviewURL = [NSString stringWithFormat:@"https://apps.apple.com/app/id%@?action=write-review", trackId];
+
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:reviewURL] options:@{} completionHandler:nil];
+
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Can't get appId"];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
