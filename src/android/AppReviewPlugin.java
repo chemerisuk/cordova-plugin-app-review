0b61f5ec7e56ce1f2851594b1bfad8e90b2b6d55
Maksim Chemerisiuk <chemerisuk@gmail.com>
2022-08-10 20:19:08 +0300

    Simplify android implementation

--- src/android/AppReviewPlugin.java
+++ src/android/AppReviewPlugin.java
@@ -1,61 +1,41 @@
 package by.chemerisuk.cordova;
 
+import static com.google.android.gms.tasks.Tasks.await;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
-import by.chemerisuk.cordova.support.CordovaMethod;
-import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
-
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.json.JSONException;
+
+import by.chemerisuk.cordova.support.CordovaMethod;
+import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
 
 public class AppReviewPlugin extends ReflectiveCordovaPlugin {
     @CordovaMethod
-    private void requestReview(final CallbackContext callbackContext) {
-        final Activity activity = cordova.getActivity();
-        final ReviewManager manager = ReviewManagerFactory.create(activity);
-
-        manager.requestReviewFlow().addOnCompleteListener(new OnCompleteListener<ReviewInfo>() {
-            @Override
-            public void onComplete(Task<ReviewInfo> task) {
-                if (task.isSuccessful()) {
-                    manager.launchReviewFlow(activity, task.getResult()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                callbackContext.success();
-                            } else {
-                                respondWith(task.getException(), callbackContext);
-                            }
-                        }
-                    });
-                } else {
-                    respondWith(task.getException(), callbackContext);
-                }
-            }
-        });
-    }
-
-    @CordovaMethod
-    private void openStoreScreen(String packageName, CallbackContext callbackContext) {
-        if (packageName == null) {
-            packageName = cordova.getActivity().getPackageName();
-        }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-        cordova.getActivity().startActivity(intent);
-
+    private void requestReview(CallbackContext callbackContext) throws Exception {
+        Activity activity = cordova.getActivity();
+        ReviewManager manager = ReviewManagerFactory.create(activity);
+        ReviewInfo reviewInfo = await(manager.requestReviewFlow());
+        await(manager.launchReviewFlow(activity, reviewInfo));
         callbackContext.success();
     }
 
-    private void respondWith(Exception e, CallbackContext callbackContext) {
-        callbackContext.error(e != null ? e.getMessage() : "Unknown error");
+    @CordovaMethod
+    protected void openStoreScreen(CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+        String packageName = args.getString(0);
+        if (packageName == null) {
+            packageName = cordova.getActivity().getPackageName();
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        cordova.getActivity().startActivity(intent);
+        callbackContext.success();
     }
 }
