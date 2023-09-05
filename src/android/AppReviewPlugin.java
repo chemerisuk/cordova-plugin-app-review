b9a4516095e7a064ee25f9275447f192c1604204
kartikthakur2022 <96222501+kartikthakur2022@users.noreply.github.com>
2023-09-05 16:49:30 +0530

    Update AppReviewPlugin.java

--- src/android/AppReviewPlugin.java
+++ src/android/AppReviewPlugin.java
@@ -2,10 +2,11 @@ package by.chemerisuk.cordova;
 
 import static com.google.android.gms.tasks.Tasks.await;
 import static by.chemerisuk.cordova.support.ExecutionThread.WORKER;
-
+import java.lang.reflect.Field;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
@@ -14,6 +15,7 @@ import com.google.android.play.core.review.ReviewManagerFactory;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
@@ -38,5 +40,44 @@ public class AppReviewPlugin extends ReflectiveCordovaPlugin {
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
         cordova.getActivity().startActivity(intent);
         callbackContext.success();
+    }
+
+    @CordovaMethod
+    protected void getDeviceOSVersion(CallbackContext callbackContext) throws Exception{
+        try{
+            JSONObject details = new JSONObject();
+            details.put("version", Build.VERSION.RELEASE);
+            details.put("apiLevel", Build.VERSION.SDK_INT);
+            details.put("apiName", getNameForApiLevel(Build.VERSION.SDK_INT));
+            callbackContext.success(details);
+        } catch(Exception e ) {
+            handleError("Exception occurred: ".concat(e.getMessage()), callbackContext);
+        }
+    }
+
+     // https://stackoverflow.com/a/55946200/777265
+    protected String getNameForApiLevel(int apiLevel) throws Exception{
+        Field[] fields = Build.VERSION_CODES.class.getFields();
+        String codeName = "UNKNOWN";
+        for (Field field : fields) {
+            if (field.getInt(Build.VERSION_CODES.class) == apiLevel) {
+                codeName = field.getName();
+            }
+        }
+        return codeName;
+    }
+
+    /**
+     * Handles an error while executing a plugin API method  in the specified context.
+     * Calls the registered Javascript plugin error handler callback.
+     * @param errorMsg Error message to pass to the JS error handler
+     */
+    public void handleError(String errorMsg, CallbackContext context){
+        try {
+            logError(errorMsg);
+            context.error(errorMsg);
+        } catch (Exception e) {
+            logError(e.toString());
+        }
     }
 }
